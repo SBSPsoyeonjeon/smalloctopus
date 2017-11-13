39b92ff17d16d9205d06760dec8ea360a0a2f874
SBSPsoyeonjeon <solainejsy@gmail.com>
2017-11-13 22:35:03 +0900

    init

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+	public string scenename;
+    //public int i;
+
+    void OnTriggerEnter2D(Collider2D other)//왕새우 먹으면
+    {
+        if (other.CompareTag("Player"))
+        {
+            //SceneManager.LoadScene("play1_2"); //이 씬 이름으로 (다음 단계로) 이동
+    		SceneManager.LoadScene(scenename);
+            
+       
+        }
+    }
+   
+}
+
+
---
+++ Assets/Scripts/Fading.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fading : MonoBehaviour {
+    /*
+    public float fadeSpeed = 0.8f; //fading speed
+
+
+    public void BeginFade()
+    {
+        StartCoroutine("_FadeIn");
+        //return (fadeSpeed);
+    }
+
+
+    IEnumerator _FadeIn()
+    {
+        for(float i=1f; i>=0; i-=0.1f)
+        {
+            Color color = new Vector4(1, 1, 1, i);
+            transform.GetComponent<Renderer>().material.color = color;
+            yield return 0;
+
+        }
+    }*/
+
+
+    public Texture2D fadeOutTexture;//검은 화면
+    public float fadeSpeed = 0.8f; //fading speed
+
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+
+    void onGUI()
+    {
+        Debug.Log(10);
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        Debug.Log(20);
+        alpha = Mathf.Clamp01(alpha);
+        Debug.Log(30);
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        Debug.Log(40);
+        GUI.depth = drawDepth;
+        Debug.Log(50);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+        Debug.Log(60);
+    }
+
+    public float BeginFade(int direction)
+    {
+      
+        fadeDir = direction;
+        return (fadeSpeed);
+    }
+
+    void OnLevelWasLoaded()
+    {
+        BeginFade(-1);
+    }
+    
+}
